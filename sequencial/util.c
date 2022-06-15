2edf4b8d6fc110ca09adeb45f7a679a10871e635
felipeesser <felipeesser@id.uff.br>
2022-06-15 15:45:36 -0300

    refatoracao codigo sequencial

---
+++ sequencial/util.c
@@ -0,0 +1,25 @@
+#include "util.h"
+
+void ler_entrada(char *nome_arq, int **v, int *tamanho_v){
+    FILE* arq = fopen(nome_arq, "rt");
+    fscanf(arq, "%d", tamanho_v);
+    *v= (int*) malloc((*tamanho_v)*sizeof(int));
+    for(int i = 0; i < *tamanho_v; i++) fscanf(arq, "%d", &(*v)[i]);
+    fclose(arq);
+}
+
+void escrever_saida(char *nome_arq, int *v, int tamanho_v, double tempo){
+    FILE* arq = fopen(nome_arq, "at");
+    fprintf(arq, "Vetor Ordenado: [");
+    for(int i = 0; i < tamanho_v; i++){
+        fprintf(arq, "%d", v[i]);
+        if(i != tamanho_v - 1) fprintf(arq, ", ");
+    }
+    fprintf(arq, "]\n");
+    fprintf(arq, "Tempo de CPU: %lf\n", tempo);
+    fclose(arq);
+}
+
+void liberar(int **v){
+    free(*v);
+}
